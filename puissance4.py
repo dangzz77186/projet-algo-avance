7052363878fa4dfdbc898ff10a5acde3e5774f38
dangzz77186 <dang-luan.ta@efrei.net>
2025-09-25 17:42:24 +0200

    quatrieme commit du code python / ajout du code principal/fonction puissance4

--- puissance4.py
+++ puissance4.py
@@ -40,3 +40,54 @@ def jouer_coup(grille, colonne, symbole):
             grille[i][colonne] = symbole
             return True
     return False
+
+def puissance4():
+    lignes = 6
+    colonnes = 7
+    grille = []
+    for i in range(lignes):
+        ligne = []
+        for j in range(colonnes):
+            ligne.append(' ')
+        grille.append(ligne)
+
+    tour = 0
+    joueurs = ['X', 'O']
+
+    while True:
+        afficher_grille(grille)
+        symbole = joueurs[tour % 2]
+
+        colonne_str = input('Joueur ' + symbole + ', entrez le numéro de la colonne (0-' + str(colonnes - 1) + '): ')
+
+        if not colonne_str.isdigit():
+            print('Entrée invalide, entrez un nombre.')
+            continue
+
+        colonne = int(colonne_str)
+
+        if colonne < 0 or colonne >= colonnes:
+            print('Colonne invalide, réessayez.')
+            continue
+
+        if not jouer_coup(grille, colonne, symbole):
+            print('Cette colonne est pleine, réessayez.')
+            continue
+
+        if verifier_victoire(grille, symbole):
+            afficher_grille(grille)
+            print('Le joueur ' + symbole + ' a gagné !')
+            break
+
+        plein = True
+        for c in range(colonnes):
+            if grille[0][c] == ' ':
+                plein = False
+                break
+        if plein == True:
+            afficher_grille(grille)
+            print('Match nul !')
+            break
+
+        tour += 1
+puissance4()
